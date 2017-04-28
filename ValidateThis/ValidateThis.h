b44441ab674796eeb18531522eda148460d6bc8e
David Sweetman <david@davidsweetman.com>
2017-04-27 21:01:14 -0400

    Initial Commit

---
+++ ValidateThis/ValidateThis.h
@@ -0,0 +1,19 @@
+//
+//  ValidateThis.h
+//  ValidateThis
+//
+//  Created by sweetman on 4/27/17.
+//  Copyright © 2017 tinfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ValidateThis.
+FOUNDATION_EXPORT double ValidateThisVersionNumber;
+
+//! Project version string for ValidateThis.
+FOUNDATION_EXPORT const unsigned char ValidateThisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidateThis/PublicHeader.h>
+
+

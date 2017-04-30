52bd44f3a946eaa70f4e73b3b59098f132285c8b
David Sweetman <david@davidsweetman.com>
2017-04-30 15:29:59 -0400

    Add travis & update some project settings. Remove UIKit from Validate.h because we don't need it.

--- Sources/Validate/Validate.h
+++ Sources/Validate/Validate.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 tinfish. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Validate.
 FOUNDATION_EXPORT double ValidateVersionNumber;
